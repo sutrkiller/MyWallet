8ccfa25e9daaaa3cfbf49d52f43428746447ac34
sutrkiller <tobias.kamenicky@gmail.com>
2016-10-16 10:55:35 +0200

    Add project files.

---
+++ src/MyWallet/Program.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="FI Muni">
+//   Tobias Kamenicky, Marek Halas, Robert Havlicek, Miroslav Gasparovic
+// </copyright>
+// <summary>
+//   Defines the Program type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MyWallet
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+
+    using Microsoft.AspNetCore.Hosting;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main method.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        [SuppressMessage("ReSharper", "TooManyChainedReferences", Justification = "Chaining is convention here.")]
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
