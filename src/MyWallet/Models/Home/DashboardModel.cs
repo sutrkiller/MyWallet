538d4d0bd8e7ca6b73625b7e688356194a893e7c
Najte <robhavlicek@gmail.com>
2016-12-04 21:18:40 +0100

    basic dashboard

---
+++ src/MyWallet/Models/Home/DashboardModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyWallet.Models.Entries;
+
+namespace MyWallet.Models.Home
+{
+    public class DashboardModel
+    {
+        public CreateEntryViewModel Entry { get; set; }
+    }
+}
