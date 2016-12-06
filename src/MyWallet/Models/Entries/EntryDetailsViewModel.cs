de9e81e7bf8051b451a7c78dcf36047fca18ebad
Najte <robhavlicek@gmail.com>
2016-12-06 21:09:23 +0100

    minor fixes

--- src/MyWallet/Models/Entries/EntryDetailsViewModel.cs
+++ src/MyWallet/Models/Entries/EntryDetailsViewModel.cs
@@ -10,14 +10,16 @@ namespace MyWallet.Models.Entries
     public class EntryDetailsViewModel
     {
         public string Amount { get; set; }
-        [DisplayName("Amount converted")]
+        [Display(Name = "Amount converted")]
         public string AmountInMain { get; set; }
         public string Description { get; set; }
+        [Display(Name = "Entry time")]
         public DateTime EntryTime { get; set; }
         [Display(Name = "User")]
         public string UserName { get; set; }
         [Display(Name = "Categories")]
         public string CategoryNames { get; set; }
-        public ICollection<BudgetDTO> Budgets { get; set; } = new HashSet<BudgetDTO>();
+        [Display(Name = "Budgets")]
+        public string BudgetsNames { get; set; }
     }
 }
